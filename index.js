417bad697456f7f5629c40b98341953c67cdf922
jckstllrd <jackstallard17@gmail.com>
2024-09-16 16:00:53 +0100

    Finalise Grid Setup

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const container = document.querySelector(".container");
 
-let dimension = 16;
+let dimension = 20;
 const width = 960;
+
+let divWidth = width / dimension;
+
+console.log(divWidth * dimension);
+
+let divHeight = divWidth;
 
 let flexBasis = 100 / dimension;
 
-let divWidth = Math.floor(width / dimension);
-let divHeight = divWidth;
+count = 1;
 
 function createGrid(dimension) {
   for (let x = 0; x < dimension; x++) {
     for (let y = 0; y < dimension; y++) {
       const div = document.createElement("div");
-      div.style.flex = "1 0 " + flexBasis + "%";
       div.style.width = divWidth + "px";
       div.style.height = divHeight + "px";
-      //temp styling
-      div.style.border = "1px solid black";
+      //   div.textContent = count;
+      //   count++;
+      //   setHover(div);
       container.appendChild(div);
     }
   }
 }
+
+// function setHover(element) {
+//   element.addEventListener("hover", () => {
+//     element.style.backgroundColor = "black";
+//   });
+// }
 
 createGrid(dimension);
